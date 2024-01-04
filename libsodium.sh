4dbf83c4905d6fe9e73f6f6e764b82c0a056cc56
su <>
2024-01-04 08:18:42 -0500

    libsodium-1.0.19

--- libsodium.sh
+++ libsodium.sh
@@ -2,5 +2,10 @@
 
 # https://doc.libsodium.org/installation#cross-compiling-to-android
 export ANDROID_NDK_ROOT=$ANDROID_HOME/ndk/$NDK_VERSION
+
+LIBSODIUM_VERSION=1.0.19
+
+git clone --recursive -b $LIBSODIUM_VERSION https://github.com/jedisct1/libsodium.git
 cd libsodium
 ./dist-build/android-aar.sh
+mv libsodium-$LIBSODIUM_VERSION.0.aar ../libsodium-$LIBSODIUM_VERSION.aar 
