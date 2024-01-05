1ddb5625f79b09f4c67c59f52f7948ffd8d8aa63
su <>
2024-01-05 08:21:23 -0500

    curl-8.5.0

--- curl_8.sh
+++ curl_8.sh
@@ -6,6 +6,8 @@ build_x86() {
   PATH=$ANDROID_NDK_HOME/toolchains/x86-4.9/prebuilt/linux-x86_64/bin:$PATH  
   ./configure --host x86 --with-pic --with-openssl --prefix=/tmp/curl/${arch}
   make && make install 
+  ./configure --host x86 --with-pic --with-openssl --disable-shared --prefix=/tmp/curl/${arch}
+  make && make install 
   make clean
 }
 
@@ -13,6 +15,8 @@ build_arm() {
   arch="arm"
   PATH=$ANDROID_NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH  
   ./configure --host arm-linux-androideabi --with-pic --with-openssl --prefix=/tmp/curl/${arch}
+  make && make install 
+  ./configure --host arm-linux-androideabi --with-pic --disable-shared --with-openssl --prefix=/tmp/curl/${arch}
   make && make install 
   make clean
 }
@@ -22,6 +26,8 @@ build_arm64() {
   PATH=$ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH  
   ./configure --host aarch64-linux-android --with-pic --with-openssl --prefix=/tmp/curl/${arch}
   make && make install 
+  ./configure --host aarch64-linux-android --with-pic --disable-shared --with-openssl --prefix=/tmp/curl/${arch}
+  make && make install 
   make clean
 }
 
@@ -29,6 +35,8 @@ build_x86_64() {
   arch="x86_64"
   PATH=$ANDROID_NDK_HOME/toolchains/x86_64-4.9/prebuilt/linux-x86_64/bin:$PATH  
   ./configure --host x86_64 --with-pic --with-openssl --prefix=/tmp/curl/${arch}
+  make && make install 
+  ./configure --host x86_64 --with-pic --disable-shared --with-openssl --prefix=/tmp/curl/${arch}
   make && make install 
   make clean
 }
