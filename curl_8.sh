832f1ab3de108abef122d1aaed07225a7ffd64d3
su <>
2024-01-05 08:08:08 -0500

    curl-8.5.0

---
+++ curl_8.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+build_x86() {
+  arch="x86"
+  PATH=$ANDROID_NDK_HOME/toolchains/x86-4.9/prebuilt/linux-x86_64/bin:$PATH  
+  ./configure --host x86 --with-pic --with-openssl --prefix=/opt/curl/${arch}
+  make && make install 
+  mkdir -p /tmp/curl/${arch} && mv /opt/curl/${arch}/* /tmp/curl/${arch}/
+  make clean
+}
+
+build_arm() {
+  arch="arm"
+  PATH=$ANDROID_NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH  
+  ./configure --host arm-linux-androideabi --with-pic --with-openssl --prefix=/opt/curl/${arch}
+  make && make install 
+  mkdir -p /tmp/curl/${arch} && mv /opt/curl/${arch}/* /tmp/curl/${arch}/
+  make clean
+}
+
+build_arm64() {
+  arch="arm64"
+  PATH=$ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH  
+  ./configure --host aarch64-linux-android --with-pic --with-openssl --prefix=/opt/curl/${arch}
+  make && make install 
+  mkdir -p /tmp/curl/${arch} && mv /opt/curl/${arch}/* /tmp/curl/${arch}/
+  make clean
+}
+
+build_x86_64() {
+  arch="x86_64"
+  PATH=$ANDROID_NDK_HOME/toolchains/x86_64-4.9/prebuilt/linux-x86_64/bin:$PATH  
+  ./configure --host x86_64 --with-pic --with-openssl --prefix=/opt/curl/${arch}
+  make && make install 
+  mkdir -p /tmp/curl/${arch} && mv /opt/curl/${arch}/* /tmp/curl/${arch}/
+  make clean
+}
+
+# Execute builds for different architectures
+export ANDROID_NDK_HOME=$ANDROID_HOME/$NDK_VERSION
+autoreconf -fi
+cd curl
+build_x86
+# build_arm
+# build_arm64
+# build_x86_64
+
