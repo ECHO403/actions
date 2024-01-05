2bd06902075343bb12810f2dbc295948badbe5ff
nullptrX <19757745+nullptrx@users.noreply.github.com>
2024-01-05 23:12:52 +0800

    curl-8.5.0

--- curl_8.sh
+++ curl_8.sh
@@ -16,17 +16,13 @@ build_arm() {
   PATH=$ANDROID_NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH  
   ./configure --host arm-linux-androideabi --with-pic --with-openssl --prefix=/tmp/curl/${arch}
   make && make install 
-  ./configure --host arm-linux-androideabi --with-pic --disable-shared --with-openssl --prefix=/tmp/curl/${arch}
-  make && make install 
   make clean
 }
 
 build_arm64() {
   arch="arm64"
   PATH=$ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH  
-  # ./configure --host aarch64-linux-android --with-pic --with-openssl --prefix=/tmp/curl/${arch}
-  # make && make install 
-  ./configure --host aarch64-linux-android --with-pic --disable-shared --with-openssl --prefix=/tmp/curl/${arch}
+  ./configure --host aarch64-linux-android --with-pic --with-openssl --prefix=/tmp/curl/${arch}
   make && make install 
   make clean
 }
@@ -36,18 +32,15 @@ build_x86_64() {
   PATH=$ANDROID_NDK_HOME/toolchains/x86_64-4.9/prebuilt/linux-x86_64/bin:$PATH  
   ./configure --host x86_64 --with-pic --with-openssl --prefix=/tmp/curl/${arch}
   make && make install 
-  ./configure --host x86_64 --with-pic --disable-shared --with-openssl --prefix=/tmp/curl/${arch}
-  make && make install 
   make clean
 }
 
 # Execute builds for different architectures
 export ANDROID_NDK_HOME=$ANDROID_HOME/$NDK_VERSION
-
 mkdir -p /tmp/curl 
 cd curl && autoreconf -fi
-# build_x86
-# build_arm
+build_x86
+build_arm
 build_arm64
-# build_x86_64
+build_x86_64
 
